96120834599a24e05082626312e2cea47edec9fd
jagath9923 <jagath9923@gmail.com>
2024-03-28 22:19:29 +0530

    added the isPublishVersion from lambda function handler

--- task02/jsrc/main/java/com/task02/HelloWorld.java
+++ task02/jsrc/main/java/com/task02/HelloWorld.java
@@ -17,7 +17,7 @@ import java.util.Map;
 
 @LambdaHandler(lambdaName = "hello_world",
         roleName = "hello_world-role",
-        //isPublishVersion = true,
+        isPublishVersion = true,
         //aliasName = "${lambdas_alias_name}",
         logsExpiration = RetentionSetting.SYNDICATE_ALIASES_SPECIFIED
 )
