039a0e163de4252e01050c5710c8c9b082b09aad
jagath9923 <jagath9923@gmail.com>
2024-03-28 21:40:45 +0530

    Removed the isPublishVersion from lambda function handler

--- task01/jsrc/main/java/com/task01/HelloWorld.java
+++ task01/jsrc/main/java/com/task01/HelloWorld.java
@@ -10,8 +10,8 @@ import java.util.Map;
 
 @LambdaHandler(lambdaName = "hello_world",
 	roleName = "hello_world-role",
-	isPublishVersion = true,
-//	aliasName = "${lambdas_alias_name}",
+	//isPublishVersion = true,
+	//aliasName = "${lambdas_alias_name}",
 	logsExpiration = RetentionSetting.SYNDICATE_ALIASES_SPECIFIED
 )
 public class HelloWorld implements RequestHandler<Object, Map<String, Object>> {
